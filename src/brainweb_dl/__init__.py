a6d095bd21bc08b9742ded6fc19f688aa7b32e9a
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-05-26 23:33:59 +0200

    feat: expose functions to download brainweb data.

--- src/brainweb_dl/__init__.py
+++ src/brainweb_dl/__init__.py
@@ -1,1 +1,11 @@
-#!/usr/bin/env python3
+from .mri import get_mri
+
+from ._brainweb import get_brainweb1, get_brainweb20, get_brainweb20_multiple
+
+
+__all__ = [
+    "get_mri",
+    "get_brainweb1",
+    "get_brainweb20",
+    "get_brainweb20_multiple",
+]
