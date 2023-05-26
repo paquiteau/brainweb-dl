8168c7061a3f15b36efd9fbd6a98cd36057f0834
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-05-27 00:09:51 +0200

    feat: expose the shape of the images.

--- src/brainweb_dl/__init__.py
+++ src/brainweb_dl/__init__.py
@@ -1,11 +1,13 @@
 from .mri import get_mri
 
 from ._brainweb import get_brainweb1, get_brainweb20, get_brainweb20_multiple
-
+from ._brainweb import STD_RES, BIG_RES
 
 __all__ = [
     "get_mri",
     "get_brainweb1",
     "get_brainweb20",
     "get_brainweb20_multiple",
+    "STD_RES",
+    "BIG_RES",
 ]
